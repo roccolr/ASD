9251b68647b78d4a7e50e2cccaf7a65459edcbde
roccolr <roccolorusso99@live.it>
2025-08-25 21:27:17 +0200

    fixed problem 10611 #1

--- UVas/10611.c
+++ UVas/10611.c
@@ -9,9 +9,12 @@ int main(int argc, char ** argv){
     int n = atoi(argv[1]);
     int q = atoi(argv[2+n]);
     int * arr = (int*)malloc(n*sizeof(int));
+    printf("input: ");
     for(int i=0; i<n; i++){
-        arr[i] = atoi(argv[1+i]);
+        arr[i] = atoi(argv[2+i]);
+        printf("%d ", arr[i]);
     }
+    printf("\n");
     for(int i=0; i<q; i++){
         find_chimps(arr, n, atoi(argv[3+n+i]), &chimp1, &chimp2);
         if(chimp1 != -1 && chimp2 != -1) printf("%d %d\n", chimp2, chimp1);
@@ -34,38 +37,34 @@ void find_chimps(int * arr, int n, int h, int * chimp1, int* chimp2){
 }
 
 void binary_search(int * arr, int low, int high, int n ,int key, int * elem1, int * elem2){
-    int mid = (low+high)/2;
-    if(arr[mid] == key){
-        *elem1 = mid+1>=n? -1:arr[mid+1];
-        *elem2 = mid-1<0? -1:arr[mid-1];
-        return;
-    }else if(low == high){
-        if(mid+1 == n && arr[mid] < key){
+    if(low>high){
+        // failed research
+        if (low == n){
             *elem1 = -1;
-            *elem2 = arr[mid];
-        }else if(mid+1 == n && arr[mid] > key){
-            *elem1 = arr[mid];
-            *elem2 = arr[mid-1];
-        }else if(mid-1<0 && arr[mid] > key){
-            *elem1 = arr[mid];
+            *elem2 = arr[high];
+        }
+        else if (high < 0){
+            *elem1 = low;
             *elem2 = -1;
-        }else if(mid-1 <0 && arr[mid] < key){
-            *elem1 = arr[mid+1];
-            *elem2 = arr[mid];
         }
-        if(arr[mid] > key){
-            *elem1 = arr[mid];
-            *elem2 = arr[mid-1];
-        }else{
-            *elem1 = arr[mid+1];
-            *elem2 = arr[mid];
+        else{
+            *elem1 = arr[low];
+            *elem2 = arr[high];
         }
-        return;
     }
-    if(arr[mid]>key){
-        binary_search(arr, low, mid-1, n, key, elem1, elem2);
-    }else{
-        binary_search(arr, mid+1, high, n, key, elem1, elem2);
+    int mid = (low+high)/2;
+    if (arr[mid] > key) binary_search(arr, low, mid-1, n, key, elem1, elem2);
+    else if (arr[mid] < key) binary_search(arr, mid+1, high, n, key, elem1, elem2);
+    else if (arr[mid]==key && mid+1 == n){
+        *elem1 = -1;
+        *elem2 = arr[mid-1]; 
     }
-    return;
+    else if (arr[mid]==key && mid-1 < 0){
+        *elem1 = arr[mid+1];
+        *elem2 = -1;
+    }
+    else if(arr[mid] == key){
+        *elem1 = arr[mid+1];
+        *elem2 = arr[mid-1];
+    }
 }
